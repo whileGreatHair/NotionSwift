d2ed305305d8e7f3cdb45e2db1b7f340a11fd7d1
Brian <whilegreathair@gmail.com>
2024-06-06 22:00:25 +0800

    update  getTitle() func

--- Sources/NotionSwift/Models/Database.swift
+++ Sources/NotionSwift/Models/Database.swift
@@ -70,7 +70,7 @@ extension Database: Identifiable {}
 
 extension Database {
     public func getTitle() -> String {
-        return title.map { $0.plainText ?? "No Title" }.joined()
+        return title.isEmpty ? "Untitled": title.map { $0.plainText ?? "" }.joined()
     }
 }
 
