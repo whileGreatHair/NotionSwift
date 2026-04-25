d763edf2f8b6932bf1173761ade342eb5d0617fe
beforeold <ibeforeold@gmail.com>
2026-04-25 16:59:43 +0800

    Add page id support to create requests

--- Sources/NotionSwift/Models/Request/PageCreateRequest.swift
+++ Sources/NotionSwift/Models/Request/PageCreateRequest.swift
@@ -5,15 +5,18 @@
 import Foundation
 
 public struct PageCreateRequest {
+    public let id: Page.Identifier?
     public let parent: PageParentType // A database parent or page parent
     public let properties: [Page.PropertyName: WritePageProperty]
     public let children: [WriteBlock]?
 
     public init(
+        id: Page.Identifier? = nil,
         parent: PageParentType,
         properties: [Page.PropertyName: WritePageProperty],
         children: [WriteBlock]? = nil
     ) {
+        self.id = id
         self.parent = parent
         self.properties = properties
         self.children = children
